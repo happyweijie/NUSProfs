0c8a1004958f7d9c507bc67bcfcb58a2bdd4704a
Wei Jie Chew <weijie.chew@gmail.com>
2025-05-28 11:53:44 +0800

    basic search functionality with pagination

--- professors/views.py
+++ professors/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Professor, Teaches, Module, Department, Faculty
 from .serializers import ProfessorSummarySerializer, ProfessorDetailSerializer
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
 
 # Create your views here.
 def index(request):
     return render(request, "professors/index.html")
 
+@api_view(['GET'])
 def all_professors(request):
+    paginator = PageNumberPagination()
+    paginator.page_size = 10
     professors = Professor.objects.all().order_by('name')
-    serializer = ProfessorSummarySerializer(professors, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    result_page = paginator.paginate_queryset(professors, request)
+    serializer = ProfessorSummarySerializer(result_page, many=True)
+    return paginator.get_paginated_response(serializer.data)
 
 def professor(request, prof_id):
     try:
@@ -21,10 +27,13 @@ def professor(request, prof_id):
     
     return JsonResponse(ProfessorDetailSerializer(prof).data, safe=False)
 
+@api_view(['GET'])
 def search(request):
-    query = request.GET.get('q', '')
+    query = request.query_params.get('q', '').strip()
     results = Professor.objects.filter(name__icontains=query).order_by('name')
-    if not results:
-        return JsonResponse({"error": "No professors found"}, status=404)
-    serializer = ProfessorSummarySerializer(results, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    
+    paginator = PageNumberPagination()
+    paginator.page_size = 20
+    result_page = paginator.paginate_queryset(results, request)
+    serializer = ProfessorSummarySerializer(result_page, many=True)
+    return paginator.get_paginated_response(serializer.data)
