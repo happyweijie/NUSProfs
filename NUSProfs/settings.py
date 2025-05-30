a9688d05e450cdd722e553cdbbeb275a370b43c9
Wei Jie Chew <weijie.chew@gmail.com>
2025-05-30 08:53:22 +0800

    hosting on render

--- NUSProfs/settings.py
+++ NUSProfs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t)h5m)(^wfignvx!4yx5+wsamz!y4=ibvp^v_^y!z*7abegn9)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["nusprofs-api.onrender.com"]
 
 
 # Application definition
