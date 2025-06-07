2b0ded1c8132671c6f0ea6a0762a8d6d619729be
Wei Jie Chew <weijie.chew@gmail.com>
2025-06-07 20:55:42 +0800

    moved SECRET KEY to env

--- NUSProfs/settings.py
+++ NUSProfs/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t)h5m)(^wfignvx!4yx5+wsamz!y4=ibvp^v_^y!z*7abegn9)'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
