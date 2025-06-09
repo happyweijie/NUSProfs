4586d18d06e4bf19da40709a9cd6476141610abc
Wei Jie Chew <weijie.chew@gmail.com>
2025-06-09 12:08:02 +0800

    Comments for url

--- login/urls.py
+++ login/urls.py
@@ -7,8 +7,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("whoami", views.whoami, name="whoami"),
     path('register/', views.RegisterView.as_view(), name='register'),
-    path("token/",views.UserTokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/",views.UserTokenObtainPairView.as_view(), name="token_obtain_pair"), # login
+    path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"), # refresh token
+    path("whoami", views.whoami, name="whoami"), # test protected test view
 ]
