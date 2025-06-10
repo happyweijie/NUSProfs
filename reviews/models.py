34efb6eb47f2a25c34969d1017d74cd857aba2fb
Wei Jie Chew <weijie.chew@gmail.com>
2025-06-10 21:06:20 +0800

    Comment model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
+from login.models import CustomUser
 
 # Create your models here.
+class Comment(models.Model):
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comments')
+    text = models.CharField(max_length=20000, blank=True, null=True)
+    is_visible = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(default=datetime.now())
