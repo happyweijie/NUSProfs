d8169bce3dbdb242fa044a7ea509d38c6e65a2d4
Wei Jie Chew <weijie.chew@gmail.com>
2025-06-13 21:22:26 +0800

    Added Review and Reply class

--- reviews/models.py
+++ reviews/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import datetime
 from login.models import CustomUser
+from professors.models import Professor, Module
 
 # Create your models here.
 class Comment(models.Model):
@@ -8,3 +9,19 @@ class Comment(models.Model):
     text = models.CharField(max_length=20000, blank=True, null=True)
     is_visible = models.BooleanField(default=True)
     timestamp = models.DateTimeField(default=datetime.now())
+    likes = models.ManyToManyField(CustomUser, related_name='+', blank=True)
+
+    class Meta:
+        abstract = True
+
+class Review(Comment):
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='reviews')
+    prof_id = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name='reviews')
+    module_id = models.ForeignKey(Module, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.FloatField(default=0.0)
+    likes = models.ManyToManyField(CustomUser, related_name='liked_reviews', blank=True)
+
+class Reply(Comment):
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='replies')
+    review_id = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='replies')
+    likes = models.ManyToManyField(CustomUser, related_name='liked_replies', blank=True)
