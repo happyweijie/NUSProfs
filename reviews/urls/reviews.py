16198dee6ed056a75b674daf263adedbba4bc76f
Wei Jie Chew <weijie.chew@gmail.com>
2025-06-19 14:00:54 +0800

    changed back path

--- reviews/urls/reviews.py
+++ reviews/urls/reviews.py
@@ -11,7 +11,7 @@ from ..views.reviews import (
 urlpatterns = [
     path("users/", UserReviewListView.as_view(), name="my_reviews"),
     path("users/<str:username>", UserReviewListView.as_view(), name="user_reviews"),
-    path("professor/<int:prof_id>", ProfessorReviewsView.as_view(), name="prof_reviews"),
+    path("professors/<int:prof_id>", ProfessorReviewsView.as_view(), name="prof_reviews"),
     path("create", ReviewCreateView.as_view(), name="create_review"),
     path("<int:pk>/edit", ReviewUpdateView.as_view(), name="edit_review"),
     path("<int:pk>/delete", ReviewDeleteView.as_view(), name="delete_review"),
