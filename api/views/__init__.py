18920e2da6fffa5c55f503c398618283615780e5
Wei Jie Chew <weijie.chew@gmail.com>
2025-06-20 13:19:13 +0800

    added back temporary paths in api views

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,3 +1,6 @@
 from .faculties_demo import faculties_demo
 from .search_demo import search_demo
 from .index import index
+from .professor import professor
+from .search import search
+from .faculties import faculties
