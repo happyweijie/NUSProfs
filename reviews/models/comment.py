283b4f5e711c664a6dfefc19752029d19f8a9171
Wei Jie Chew <weijie.chew@gmail.com>
2025-06-21 20:36:12 +0800

    changed datetime field to auto add now

--- reviews/models/comment.py
+++ reviews/models/comment.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class Comment(models.Model):
     text = models.CharField(max_length=20000, blank=True, null=True)
     is_visible = models.BooleanField(default=True)
-    timestamp = models.DateTimeField(default=timezone.now)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
