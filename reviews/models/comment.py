1743b7813743cd6eb9fdecc1defc77243bf5ebdc
Wei Jie Chew <weijie.chew@gmail.com>
2025-06-21 20:32:18 +0800

    Fixed bug with timestamps field in reviews/replies

--- reviews/models/comment.py
+++ reviews/models/comment.py
@@ -1,10 +1,10 @@
 from django.db import models
-from datetime import datetime
+from django.utils import timezone
 
 class Comment(models.Model):
     text = models.CharField(max_length=20000, blank=True, null=True)
     is_visible = models.BooleanField(default=True)
-    timestamp = models.DateTimeField(default=datetime.now())
+    timestamp = models.DateTimeField(default=timezone.now)
 
     class Meta:
         abstract = True
