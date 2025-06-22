27ea2658756d7bf65b3f29ee4a41cdf7c0c1c60a
Wei Jie Chew <weijie.chew@gmail.com>
2025-06-22 21:46:40 +0800

    added logout view

--- login/urls.py
+++ login/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('register', views.RegisterView.as_view(), name='register'),
     path("token",views.UserTokenObtainPairView.as_view(), name="token_obtain_pair"), # login
     path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"), # refresh token
+    path('logout', views.LogoutView.as_view(), name='logout'), # logout
     path('change_username', views.ChangeUsernameView.as_view(), name='change_username'), # change username
     path("change_password", views.ChangePasswordView.as_view(), name="change_password"), # change password
     path("whoami", views.profile, name="whoami"), # test protected test view
