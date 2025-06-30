cbf7ac06da80aa5a03f340ee499f88c34879e71d
Wei Jie Chew <weijie.chew@gmail.com>
2025-06-30 20:36:48 +0800

    Top n  Professors

--- professors/views/__init__.py
+++ professors/views/__init__.py
@@ -3,3 +3,4 @@ from .professor_details import ProfessorDetailsView
 from .faculties_list import FacultyListView
 from .professor_search import ProfessorSearchView
 from .module_list import ModuleListView
+from .top_professors import TopProfessorsView
