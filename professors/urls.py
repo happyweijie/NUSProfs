c11f7d0d15e6a49ac67c9f98ab88391f86acd24e
Wei Jie Chew <weijie.chew@gmail.com>
2025-07-01 10:21:58 +0800

    Compare professors view

--- professors/urls.py
+++ professors/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('top', views.TopProfessorsView.as_view(), name='top_professors'),
     path('search', views.ProfessorSearchView.as_view(), name='search'),
     path('faculties', views.FacultyListView.as_view(), name='faculties'),
-    path('modules', views.ModuleListView.as_view(), name='modules'),   
+    path('modules', views.ModuleListView.as_view(), name='modules'),
+    path('modules/<str:module_code>/compare', 
+         views.CompareModuleProfessorsView.as_view(), name='compare_professors'),   
 ]
