3770215e341b5dd420142d199175caec75aa9b5e
Wei Jie Chew <weijie.chew@gmail.com>
2025-07-02 17:03:22 +0800

    Scraped data for AY25/26

--- scrapers/helpers.py
+++
@@ -1,6 +0,0 @@
-def reformat_name(name): # Reformats names from "Last, First" to "First Last"
-    if "," in name:
-        last, first = name.split(", ")
-        if last and first:
-            return f"{first.strip()} {last.strip()}"
-    return name
