1f771efe00ebe1430926871725504315a8cde58c
Wei Jie Chew <weijie.chew@gmail.com>
2025-07-15 11:59:22 +0800

    Refactored prof detail serializer to inherit from summary serializer

--- professors/serializers/professor_detail_serializer.py
+++ professors/serializers/professor_detail_serializer.py
@@ -1,46 +1,29 @@
-from ..models import Professor, Teaches
+from ..models import Teaches
 from .teaches_serializer import TeachesSerializer
+from .professor_summary_serializer import ProfessorSummarySerializer
 from rest_framework import serializers
 from collections import defaultdict
 
-class ProfessorDetailSerializer(serializers.ModelSerializer):
-    average_rating = serializers.SerializerMethodField()
+class ProfessorDetailSerializer(ProfessorSummarySerializer):
     review_count = serializers.SerializerMethodField()
-    faculty = serializers.SerializerMethodField()
-    department = serializers.SerializerMethodField()
     teaching = serializers.SerializerMethodField()
     teaching_history = serializers.SerializerMethodField()
 
-    class Meta:
-        model = Professor
-        fields = [
-            'prof_id', 'name', 'title', 'office', 'phone',
-            'average_rating', 'review_count', 'faculty', 
-            'department', 'teaching', 'teaching_history'
+    class Meta(ProfessorSummarySerializer.Meta):
+        fields = ProfessorSummarySerializer.Meta.fields + [
+            'office', 'phone',
+            'review_count', 'teaching', 'teaching_history'
         ]
 
     def get_review_count(self, obj):
         return obj.review_count()
-    
-    def get_average_rating(self, obj):
-        return round(obj.average_rating(), 2)
 
-    def get_faculty(self, obj):
-        return obj.department.faculty.name if (obj.department and obj.department.faculty) else None
-
-    def get_department(self, obj):
-        return obj.department.name if obj.department else None
-    
     def get_teaching(self, obj):
-        teaches_queryset = obj.teaching.select_related('module') \
-            .order_by('semester')
+        teaches_queryset = obj.teaching.select_related('module').order_by('semester')
         return TeachesSerializer(teaches_queryset, many=True).data
 
     def get_teaching_history(self, obj):
-        teaches_queryset = Teaches.objects.filter(prof=obj) \
-            .order_by("semester") \
-            .select_related('module')
-        
+        teaches_queryset = Teaches.objects.filter(prof=obj).order_by("semester").select_related('module')
         modules = defaultdict(lambda: {"module_name": "", "semesters": []})
 
         for teach in teaches_queryset:
