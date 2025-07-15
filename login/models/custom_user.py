decb52f37557d875e7c33cb17feb1c43e548d889
Wei Jie Chew <weijie.chew@gmail.com>
2025-07-15 16:13:12 +0800

    removed photo url from User model

--- login/models/custom_user.py
+++ login/models/custom_user.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    photo_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.username
