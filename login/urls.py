dc3ab43369a5bd9d6852ac8d8719ebfdd175f5e0
Wei Jie Chew <weijie.chew@gmail.com>
2025-07-18 10:28:06 +0800

    added token verify view

--- login/urls.py
+++ login/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from . import views
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
+    TokenVerifyView
 )
 
 urlpatterns = [
     path('register', views.RegisterView.as_view(), name='register'),
     path("token",views.UserTokenObtainPairView.as_view(), name="token_obtain_pair"), # login
     path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"), # refresh token
+    path("token/verify", TokenVerifyView.as_view(), name="token_verify"), # verify token
     path('logout', views.LogoutView.as_view(), name='logout'), # logout
     path('change_username', 
          views.ChangeUsernameView.as_view(), name='change_username'), # change username
