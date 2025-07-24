5a3f6f43d858bf63e439ecff720ecde63f3196fa
Wei Jie Chew <weijie.chew@gmail.com>
2025-07-24 12:15:35 +0800

    removed comment

--- professors/models/faculty.py
+++ professors/models/faculty.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# ===== Faculty and Department =====
 class Faculty(models.Model):
     faculty_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=150)
