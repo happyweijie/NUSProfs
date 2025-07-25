7dda93fde9620ea7ea7f77563d1729050f5b3241
Wei Jie Chew <weijie.chew@gmail.com>
2025-07-25 22:58:28 +0800

    edited long line of code

--- reviews/models/review.py
+++ reviews/models/review.py
@@ -6,7 +6,8 @@ from .comment import Comment
 class Review(Comment):
     user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='reviews')
     prof_id = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name='reviews')
-    module_code = models.ForeignKey(Module, on_delete=models.CASCADE, related_name='reviews', blank=True, null=True)
+    module_code = models.ForeignKey(Module, on_delete=models.CASCADE, related_name='reviews', 
+                                    blank=True, null=True)
     rating = models.FloatField(default=0.0)
     likes = models.ManyToManyField(CustomUser, related_name='liked_reviews', blank=True)
 
